0a473da6d70ab2f58537eee5292129304b21862b
Anders Blehr <anders@andersblehr.co>
2019-10-13 13:54:16 +0200

    Generate Jazzy docs

---
+++ jazzit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+jazzy \
+    --author 'Anders Blehr' \
+    --author_url 'https://twitter.com/andersblehr' \
+    --github_url 'https://github.com/andersblehr/JSONCache' \
+    --module 'JSONCache' \
+    --source-directory . \
+    --readme 'README.md' \
+    --output docs/ \
